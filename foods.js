e77687e477c8022560302875bc1bb544871d28e3
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2021-02-08 22:41:30 +0600

    i'm a losser

--- foods.js
+++ foods.js
@@ -16,38 +16,37 @@ const foodName = foods => {
    foodDetail.innerHTML= "";
 
   (foods.meals).forEach(food => {
-
-   
     const foodDiv = document.createElement("div");
     foodDiv.className = "food";
-    
-
     const foodInfo = `
         <div class="card full-card" style="width: 13rem;">
-        <img onclick="displayFoodDetail('${food.strIngredient1}')" src=${food.strMealThumb} class="card-img-top" alt="meal">
+        <img onclick="displayFoodDetail('${food.strIngredient1},${food.strIngredient2},${food.strIngredient3},${food.strIngredient4},${food.strIngredient5},${food.strIngredient6},${food.strIngredient7},${food.strIngredient8},${food.strIngredient9},${food.strIngredient10}')" src=${food.strMealThumb} class="card-img-top" alt="meal">
         <div class="card-body card-text">
         <h3 class="card-text foodName"> ${food.strMeal}</h3>
         </div>
         </div>
-        
-       
-      `
+        `
     foodDiv.innerHTML = foodInfo;
     foodsDiv.appendChild(foodDiv);
  });
 
 }
 
+const displayFoodDetail =(name) =>{
 
-const displayFoodDetail = name =>{
-
- 
- console.log( name);
-
+console.log( name);
 const foodDetail= document.getElementById("food-detail")
- 
-  foodDetail.innerHTML=`
-  <h3>${name}</h3>
+foodDetail.innerHTML=`
+
+
+
+<div class="card text-dark bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-header"><h3>INGREDIENTS</h3></div>
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"><h5>${name}</h5></p>
+  </div>
+</div>
 
 
   `
