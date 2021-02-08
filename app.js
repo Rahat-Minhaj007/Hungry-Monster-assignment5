d6cc6618deeb89e346843d4449c48e978e9c0dcf
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2021-02-08 20:45:59 +0600

    i'm a losser

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ button.addEventListener('click', function () {
 const foodName = foods => {
   const foodsDiv = document.getElementById("foods-name");
    foodsDiv.innerHTML= "";
-   const foodDetail = document.getElementById("food-detail");
-   foodDetail.innerHTML= "";
-
   (foods.meals).forEach(food => {
 
    
@@ -24,7 +21,7 @@ const foodName = foods => {
 
     const foodInfo = `
         <div class="card full-card" style="width: 13rem;">
-        <img onclick="displayFoodDetail('${food.strIngredient1}')" src=${food.strMealThumb} class="card-img-top" alt="meal">
+        <img onclick="displayFoodDetail('${[food.strIngredient1,food.strIngredient2,food.strIngredient3,food.strIngredient4,food.strIngredient5,food.strIngredient6,food.strIngredient7,food.strIngredient8,food.strIngredient9,food.strIngredient10]}')" src=${food.strMealThumb} class="card-img-top" alt="meal">
         <div class="card-body card-text">
         <h3 class="card-text foodName"> ${food.strMeal}</h3>
         </div>
@@ -40,20 +37,17 @@ const foodName = foods => {
 
 
 const displayFoodDetail = name =>{
-
- 
  console.log( name);
+//   for (let i = 0; i < name.length; i++) {
+//   const ingredient = name[i];
+//   console.log(ingredient);
+  
+// }
 
 const foodDetail= document.getElementById("food-detail")
- 
+foodDetail.innerHTML="";
   foodDetail.innerHTML=`
   <h3>${name}</h3>
-
-
   `
 }
-
-
-
-
 
